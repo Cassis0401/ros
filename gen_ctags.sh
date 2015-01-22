4db4ded167a744dfefc95faa1f755bf682a5fdb4
Dusan Krajcovic <dusan.krajcovic@monetplus.cz>
2015-01-22 15:15:55 +0100

    Don't save tags.

--- gen_ctags.sh
+++ gen_ctags.sh
@@ -1,6 +1,5 @@
 
-rm tags
 rm .tags
+rm .tags_sorted_by_file
 
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q /opt/ros/indigo/include/
-mv tags .tags
+ctags -R --c++-kinds=+p --fields=+iaS --extra=+q /opt/ros/indigo/include/ .tags
